bfa2bad56612d3954da280452e2e533ece504d6e
Elise Jackson <elise.jackson@byu.edu>
2018-03-13 16:30:06 -0600

    Forecast Stats 1.0

--- Forecast Stats.py
+++ Forecast Stats.py
@@ -11,6 +11,8 @@ import numpy as np
 import os
 from merge_data_script import merge_data
 import re
+import hydrostats as hs
+import scipy
 import csv
 import datetime as dt
 
@@ -117,16 +119,120 @@ def combine_forecasts(folderpath):
 def ensemble_stats(file):
     return file
 
-def error_stats(df):
-    return df
+def error_stats(forecast_datapath,forecast_number,output_datapath):
+    #format dataframe for metrics
+    df = pd.read_csv(forecast_datapath)
+    df['recorded streamflow'] = df['recorded streamflow'].astype(np.float64)
 
+    # Replace 0's with .001
+    df = df.replace(to_replace=0, value=0.001)
+    df = df.replace(to_replace=1, value=1.0001)
 
-# counter=1
-# for i in spt_forecast:
-#     forecast=folderpath + str(i)
-#     print(forecast)
-#     data_format(forecast,counter)
-#     counter+=1
+    # Log Transform Data
+    df['log Predicted'] = np.log(df['predicted streamflow'])
+    df['log Recorded'] = np.log(df['recorded streamflow'], dtype='float64')
+
+    # Account for any negatives:
+    if min(df['log Predicted']) < 0 or min(df['log Recorded']) < 0:
+        adjustment = max(abs(min(df['log Predicted'])), abs(min(df['log Recorded']))) + 1
+        print(adjustment)
+        df['log Predicted'] = df['log Predicted'] + adjustment
+        df['log Recorded'] = df['log Recorded'] + adjustment
+    else:
+        adjustment = 0
+
+    ## create Dataframe columns
+    cor_coeff = {}
+    mean_diff = {}
+    mean_var = {}
+    month_predicted = {}
+    month_observed = {}
+    rmse = {}
+    rmse_log = {}
+    NS_eff = {}
+    R2 = {}
+    sa = {}
+
+    #Total Metrics
+    log_pred = df.dropna()['log Predicted']
+    log_observed = df.dropna()['log Recorded']
+    cor_coeff[1] = hs.acc(log_observed, log_pred)
+    mean_diff[1] = np.exp(scipy.stats.gmean(log_observed) - scipy.stats.gmean(log_pred))
+    mean_var[1] = np.var(log_observed - log_pred)
+    month_predicted[1] = np.exp(np.mean(log_pred - adjustment))
+    month_observed[1] = np.exp(np.mean(log_observed - adjustment))
+
+    #Error Metrics
+    predicted = df.dropna()['predicted streamflow']
+    observed = df.dropna()['recorded streamflow']
+    log_predicted = df.dropna()['log Predicted']
+    log_observed = df.dropna()['log Recorded']
+    n = predicted.count()
+    rmse[1] = hs.rmse(predicted, observed)
+    rmse_log[1] = hs.rmsle(log_predicted, log_observed)
+    NS_eff[1] = hs.E(log_predicted, log_observed)
+    R2[1] = hs.r_squared(predicted, observed)
+    sa[1] = hs.sa(predicted, observed)
+
+    #Combine into one dataframe
+    cor_coeff_df = pd.DataFrame.from_dict(cor_coeff, orient='Index')
+    mean_df = pd.DataFrame.from_dict(mean_diff, orient='Index')
+    variance_df = pd.DataFrame.from_dict(mean_var, orient='Index')
+    predicted_df = pd.DataFrame.from_dict(month_predicted, orient='Index')
+    observed_df = pd.DataFrame.from_dict(month_observed, orient='Index')
+    correlation_df = pd.concat([cor_coeff_df, mean_df, observed_df, predicted_df, variance_df], axis=1)
+    correlation_df.columns = ['Correlation', 'Mean Difference', 'Observed Flow', 'Predicted Flow',
+                              'Mean Variance']
+    rmse_df = pd.DataFrame.from_dict(rmse, orient='Index')
+    rmse_log_df = pd.DataFrame.from_dict(rmse_log, orient='Index')
+    NS_eff_df = pd.DataFrame.from_dict(NS_eff, orient='Index')
+    R2_df = pd.DataFrame.from_dict(R2, orient='Index')
+    sa_df = pd.DataFrame.from_dict(sa, orient='Index')
+    error_df = pd.concat([rmse_df, rmse_log_df, NS_eff_df, R2_df, sa_df], axis=1)
+    error_df.columns = ['RMSE', 'Log RMSE', 'Nash-Sutcliffe Efficiency', 'R^2 Coefficient', 'Spectral Angle']
+    results_df = pd.concat([correlation_df, error_df], axis=1)
+
+    results_df['Forecast']="Forecast "+str(forecast_number)
+    print(results_df)
+
+    #print results to .csv
+
+    datapath_results=output_datapath+"Forecast_"+str(forecast_number)+".csv"
+    print(datapath_results)
+    results_df.to_csv(datapath_results,sep=',', index=False)
+    return results_df
+
+def combine_csvs(folderpath):
+    results=glob.glob(folderpath+"Forecast_*")
+    results.sort(key=sort_key)
+    df_list=[]
+    for file in results:
+        df_list.append(pd.read_csv(file))
+    df_results=pd.concat(df_list)
+    df_results.to_csv(folderpath+"Statistical Summary.csv", sep=',', index=False)
+
+counter=1
+for i in spt_forecast:
+    forecast=folderpath + str(i)
+    print(forecast)
+    data_format(forecast,counter)
+    counter+=1
 results_path=basepath+'\\Results\\Merged Files\\'
 combine_forecasts(results_path)
 
+forecasts=[os.path.basename(x) for x in glob.glob(results_path + "*_merged.csv")]
+forecasts.sort(key=sort_key)
+
+stats_summary_path=basepath+'\\Results\\Summary Files\\'
+if not os.path.exists(basepath+'\\Results\\Summary Files\\'):
+    os.makedirs(basepath+'\\Results\\Summary Files\\')
+
+forecast_number=1
+
+for i in forecasts:
+    input_datapath=results_path+str(i)
+    error_stats(input_datapath,forecast_number,stats_summary_path)
+    forecast_number+=1
+    print(forecast_number)
+
+combine_csvs(stats_summary_path)
